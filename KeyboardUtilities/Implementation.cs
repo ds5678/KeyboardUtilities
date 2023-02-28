db7598194cee1c44429180592c0f781b8cad0d98
Jeremy Pritts <49847914+ds5678@users.noreply.github.com>
2023-02-28 14:37:39 -0500

    Add nuget package generation and bump version

--- KeyboardUtilities/Implementation.cs
+++ KeyboardUtilities/Implementation.cs
@@ -8,7 +8,7 @@ internal static class BuildInfo
 	public const string Description = "A input system mod."; // Description for the Mod.  (Set as null if none)
 	public const string Author = "Sinai, ds5678"; // Author of the Mod.  (MUST BE SET)
 	public const string Company = null; // Company that made the Mod.  (Set as null if none)
-	public const string Version = "1.3.0"; // Version of the Mod.  (MUST BE SET)
+	public const string Version = "2.0.0"; // Version of the Mod.  (MUST BE SET)
 	public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
 }
 internal class Implementation : MelonMod
