1ee52429658652ba1d3abef2da5ad98aaa4d201e
Jeremy Pritts <49847914+ds5678@users.noreply.github.com>
2023-02-28 14:27:44 -0500

    Add test mod

---
+++ SpacebarTestMod/Implementation.cs
@@ -0,0 +1,32 @@
+﻿using KeyboardUtilities;
+using MelonLoader;
+using UnityEngine;
+
+namespace SpacebarTestMod;
+public static class BuildInfo
+{
+	public const string Name = "SpacebarTestMod"; // Name of the Mod.  (MUST BE SET)
+	public const string Description = "A test mod for KeyboardUtilities."; // Description for the Mod.  (Set as null if none)
+	public const string Author = "ds5678"; // Author of the Mod.  (MUST BE SET)
+	public const string Company = null; // Company that made the Mod.  (Set as null if none)
+	public const string Version = "1.0.0"; // Version of the Mod.  (MUST BE SET)
+	public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
+}
+internal class Implementation : MelonMod
+{
+	public override void OnUpdate()
+	{
+		if (InputManager.GetKeyDown(KeyCode.Space))
+		{
+			LoggerInstance.Msg("Pressed");
+		}
+		else if (InputManager.GetKeyUp(KeyCode.Space))
+		{
+			LoggerInstance.Msg("Released");
+		}
+		else if (InputManager.GetKey(KeyCode.Space))
+		{
+			LoggerInstance.Msg("Held");
+		}
+	}
+}
---
+++ SpacebarTestMod/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using MelonLoader;
+using System.Reflection;
+
+[assembly: AssemblyTitle(SpacebarTestMod.BuildInfo.Name)]
+[assembly: AssemblyDescription(SpacebarTestMod.BuildInfo.Description)]
+[assembly: AssemblyCompany(SpacebarTestMod.BuildInfo.Company)]
+[assembly: AssemblyProduct(SpacebarTestMod.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + SpacebarTestMod.BuildInfo.Author)]
+[assembly: AssemblyTrademark(SpacebarTestMod.BuildInfo.Company)]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion(SpacebarTestMod.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(SpacebarTestMod.BuildInfo.Version)]
+[assembly: MelonInfo(typeof(SpacebarTestMod.Implementation), SpacebarTestMod.BuildInfo.Name, SpacebarTestMod.BuildInfo.Version, SpacebarTestMod.BuildInfo.Author, SpacebarTestMod.BuildInfo.DownloadLink)]
+[assembly: MelonGame(null, null)]
